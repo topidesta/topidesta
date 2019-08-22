f46fa02a89bf24d75f54d9b77a2f30f0bbf2a30d
Topidesta a.k.a EmangDasar <desta.08b@gmail.com>
2019-08-22 12:01:06 +0700

    Revert "tambah embed gist"
    This reverts commit 39d8dfa1b08422c22cd703c396e53f22693ab2d6.

--- gatsby-config.js
+++ gatsby-config.js
@@ -48,21 +48,8 @@ module.exports = {
             options: {
               width: 660,
               ratio: 1.77, // Optional: Defaults to 16/9 = 1.77
-              related: false, // Optional: Will remove related videos from the end of an embedded YouTube video.
-              noIframeBorder: true // Optional: Disable insertion of <style> border: 0
-            }
-          },
-          {
-            resolve: "gatsby-remark-embed-gist",
-            options: {
-              // Optional:
-    
-              // the github handler whose gists are to be accessed
-              username: 'mdestafadilah',
-    
-              // a flag indicating whether the github default gist css should be included or not
-              // default: true
-              includeDefaultCss: true
+              related: false, //Optional: Will remove related videos from the end of an embedded YouTube video.
+              noIframeBorder: true //Optional: Disable insertion of <style> border: 0
             }
           },
           {
