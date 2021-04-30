2fb3729a925d2a874fbb0f7b49345adbe2529122
mdestafadilah <desta.08b@gmail.com>
2021-05-01 04:13:13 +0700

    test

--- src/cms/preview/PagePreview.jsx
+++ src/cms/preview/PagePreview.jsx
@@ -2,15 +2,9 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { PageTemplate } from '../../templates/page-template'
 
-const PagePreview = ({ entry, widgetFor }) => {
-  const tags = entry.getIn(['data', 'tags'])
+const PagePreview = () => {
   return (
-    <PageTemplate
-      content={widgetFor('body')}
-      description={entry.getIn(['data', 'description'])}
-      tags={tags && tags.toJS()}
-      title={entry.getIn(['data', 'title'])}
-    />
+    <PageTemplate />
   )
 }
 
