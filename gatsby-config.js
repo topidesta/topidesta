a81dc211b7e7176d047b49958c3c16c161ae70f2
mdestafadilah <desta.08b@gmail.com>
2021-06-03 11:50:39 +0700

    Install Plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -70,6 +70,7 @@ module.exports = {
           {
             resolve: "gatsby-remark-responsive-iframe"
           },
+          "gatsby-remark-images-zoom",
           "gatsby-remark-prismjs",
           "gatsby-remark-copy-linked-files",
           "gatsby-remark-autolink-headers"
