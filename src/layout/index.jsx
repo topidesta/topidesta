8019205a2c152314ea1282d754b8611ca11c4afa
mdestafadilah <desta.08b@gmail.com>
2022-12-13 15:16:53 +0700

    TEST PAKE PERSHABATAN.CO.ID

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -19,8 +19,16 @@ const MainLayout = ({ children, hasFooter = true }) => (
       {/* <script src="https://cdn.jsdelivr.net/gh/anshumanfauzdar/contact-form@v3/src/contact-form.js" id="contactform" form_worker_url="https://v1.formtopidesta.workers.dev/"></script> */}
       <script>
         window.intergramId = "717698723";
+        window.intergramServer  = "https://telegram.persahabatan.co.id";
+        window.intergramCustomizations.titleClosed = "Tutup Obrolan";
+        window.intergramCustomizations.titleOpen = "Buka Obrolan";
+        window.intergramCustomizations.introMessage = "Selamat datang di Blog Programmer Gadungan";
+        window.intergramCustomizations.autoResponse = "Pesan akan segera terkirim..!!";
+        window.intergramCustomizations.autoNoResponse = "Pesan tidak terkirim..!!";
+        window.intergramCustomizations.mainColor = "#E91E63";
+        window.intergramCustomizations.alwaysUseFloatingButton = "false";
     </script>
-    <script id="intergram" type="text/javascript" src="https://www.intergram.xyz/js/widget.js"></script>
+    <script id="intergram" type="text/javascript" src="https://telegram.persahabatan.co.id/js/widget.js"></script>
 
     </Helmet>
     <Navigation
