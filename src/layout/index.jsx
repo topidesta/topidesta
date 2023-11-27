fa21b30a88bfcd897ba65a30130ee4097d792a7d
mDestaFadilah <desta.08b@gmail.com>
2023-11-27 09:07:57 +0700

    safe.script loaded

--- src/layout/index.jsx
+++ src/layout/index.jsx
@@ -18,27 +18,24 @@ const MainLayout = ({ children, hasFooter = true }) => (
       <link rel="shortcut icon" type="image/x-icon" href="/favicon.ico" />
       <link rel="canonical" href={useSlash(siteConfig.siteUrl)} />
       {/* <script src="https://cdn.jsdelivr.net/gh/anshumanfauzdar/contact-form@v3/src/contact-form.js" id="contactform" form_worker_url="https://v1.formtopidesta.workers.dev/"></script> */}
-      <script>
-        window.intergramId = "717698723";
-        window.intergramServer = "https://telegram.dev.persahabatan.co.id";
-    </script>
+      <Safe.script src="https://telegram.dev.persahabatan.co.id"></Safe.script>
 
-    <Safe.script>{
-      `
-      window.intergramId = "717698723";
-      window.intergramCustomizations = {
-          titleClosed : "Klik Untuk Bertanya!",
-          titleOpen : "Buka Obrolan",
-          introMessage : "Selamat datang di Blog Programmer Gadungan",
-          autoResponse : "Pesan akan segera terkirim..!!",
-          autoNoResponse : "Pesan tidak terkirim..!!",
-          mainColor : "#434348",
-          alwaysUseFloatingButton : false
-        }
-      `
-    }
-    </Safe.script>
-    <script id="intergram" type="text/javascript" src="https://telegram.dev.persahabatan.co.id/js/widget.js"></script>
+      <Safe.script>{
+        `
+        window.intergramId = "717698723";
+        window.intergramCustomizations = {
+            titleClosed : "Klik Untuk Bertanya!",
+            titleOpen : "Buka Obrolan",
+            introMessage : "Selamat datang di Blog Programmer Gadungan",
+            autoResponse : "Pesan akan segera terkirim..!!",
+            autoNoResponse : "Pesan tidak terkirim..!!",
+            mainColor : "#434348",
+            alwaysUseFloatingButton : false
+          }
+        `
+      }
+      </Safe.script>
+      <script id="intergram" type="text/javascript" src="https://telegram.dev.persahabatan.co.id/js/widget.js"></script>
 
     </Helmet>
     <Navigation
